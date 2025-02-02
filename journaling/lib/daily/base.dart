7093ec3096b90b57134e4347c429989e39660506
hwisley0619 <hwisley0619@gmail.com>
2025-02-02 20:08:36 +0900

    FIX: file structure refactoring
    - baseline for code organization
    Placed main in the top-level directory
    Created folders for major features and organized code within them
    
    I structured and linked directories accordingly. I'll continue development with this structure

--- journaling/lib/daily/base.dart
+++ journaling/lib/daily/base.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:journaling/daily/hourly_plan.dart';
 
-void main() {
-  runApp(HourlyPlan());
+class DailyBase extends StatelessWidget {
+  const DailyBase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Hello, world!'),
+        HourlyPlan(),
+      ],
+    );
+  }
 }

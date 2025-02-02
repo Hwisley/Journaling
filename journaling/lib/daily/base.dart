3ff4572b090840b5060ec262dc69352f1e0c9353
hwisley0619 <hwisley0619@gmail.com>
2025-02-02 16:25:39 +0900

    CHORE: add baseline code

---
+++ journaling/lib/daily/base.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:journaling/daily/hourly_plan.dart';
+
+void main() {
+  runApp(HourlyPlan());
+}
---
+++ journaling/lib/daily/hourly_plan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HourlyPlan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue, // Change the background color here
+      child: const Center(
+        child: Text(
+          'Hello, world!',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+}
